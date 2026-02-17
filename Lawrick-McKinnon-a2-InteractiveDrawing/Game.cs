a43dabe7a5873f13d6c139f5021b14818e5bca90
ChristopherMcKinnon <christopher.lawrick-mckinnon@mohawkcollege.ca>
2026-02-16 19:44:27 -0500

    Add proper resolution scaling for objectspace

--- Lawrick-McKinnon-a2-InteractiveDrawing/Game.cs
+++ Lawrick-McKinnon-a2-InteractiveDrawing/Game.cs
@@ -11,8 +11,11 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-        static int[] canvasSize = [600,600];
+        static int[] canvasSize = [900,600];
         static int[][] myList = [[12],[34]];
+        int scale;
+        int screenDiffX = 0;
+        int screenDiffY = 0;
         
 
         
@@ -24,69 +27,26 @@ namespace MohawkGame2D
         
         public Vector2 TransformVerticies(Vector2 v)
         {
-            
-            float xFactor = (canvasSize[0]) / 2;
-            float yFactor = (canvasSize[1]) / 2;
+            // Not sure if i can use Math.Min() here, so I made my own check
+            if (canvasSize[0] < canvasSize[1])
+            {
+                scale = canvasSize[0]/2;
+                screenDiffY = (canvasSize[1] - canvasSize[0])/2;
+            } else
+            {
+                scale = canvasSize[1]/2;
+                screenDiffX = (canvasSize[0] - canvasSize[1])/2;
+            }
 
             // Math is x*(w/2)+w/2  >   (x+1)(w/2)
 
-            v.X = (v.X + 1.0f) * xFactor;
-            v.Y = (v.Y + 1.0f) * yFactor;
+            v.X = (v.X + 1.0f) * (scale + screenDiffX);
+            v.Y = (v.Y + 1.0f) * (scale + screenDiffY);
 
             return v;
         }
 
-        public void DrawCube(float size)
-        {
-            float side = (size / 2.0f);
-            /*
-            Vector3[] verticies = [
-                new Vector3(-side, -side, -side),   // 0
-                new Vector3(side, -side, -side),    // 1
-                new Vector3(-side, side, -side),    // 2
-                new Vector3(side, side, -side),     // 3
-                new Vector3(-side, -side, side),    // 4
-                new Vector3(side, -side, side),     // 5
-                new Vector3(-side, side, side),     // 6
-                new Vector3(side, side, side)       // 7
-                ];
-            Vector2[] verticies = [
-                new Vector2(-side / -side, -side / -side),   // 0
-                new Vector2(side / -side, -side / -side),    // 1
-                new Vector2(-side / -side, side / -side),    // 2
-                new Vector2(side / -side, side / -side),     // 3
-                new Vector2(-side / side, -side / side),    // 4
-                new Vector2(side / side, -side / side),     // 5
-                new Vector2(-side / side, side / side),     // 6
-                new Vector2(side / side, side / side)       // 7
-                ];
-             */
-            Vector2[] verticies = [
-                new Vector2(-side, -side),   // 0
-                new Vector2(side, -side),    // 1
-                new Vector2(-side, side),    // 2
-                new Vector2(side, side),     // 3
-                new Vector2(-side, -side),    // 4
-                new Vector2(side, -side),     // 5
-                new Vector2(-side, side),     // 6
-                new Vector2(side, side)       // 7
-                ];
-
-            int[][] assignPoints = [
-                [0, 1], [1, 3], [3, 2], [2, 0],
-                [0, 4], [1, 5], [3, 7], [2, 6],
-                [4, 6], [5, 7], [7, 6], [6, 4]
-                ];
-            for (int i = 0; i < verticies.Length; i++)
-            {
-                verticies[i] = TransformVerticies(verticies[i]);
-            }
-            for (int i = 0; i < assignPoints.Length; i++)
-            {
-                Draw.Line(verticies[assignPoints[i][0]], verticies[assignPoints[i][1]]);
-            }
-        }
-
+        
         public void Setup()
         {
             Window.SetSize(canvasSize[0], canvasSize[1]);
@@ -101,8 +61,11 @@ namespace MohawkGame2D
         public void Update()
         {
             Window.ClearBackground(Color.White); // Clear screen
-            DrawCube(1f);
 
+            Draw.Polygon(TransformVerticies(new Vector2(0, 0)), 25, 5, 0, 0);
+            int[] list = [100,200];
+            int[] list2 = [400,800];
+            Draw.PolyLine(list, list2);
             // Convert every verticy to screenspace
             
         }
